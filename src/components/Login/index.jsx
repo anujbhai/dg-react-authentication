341fc5205f75b5a28af0da8aead9762d78507c09
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2023-09-02 12:18:05 +0530

    dg-react-auth-03-protected-routes - auth component v2.

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -8,6 +8,7 @@ const LOGIN_URL = '/auth'
 
 const Login = () => {
   const { setAuth } = useAuth()
+
   const navigate = useNavigate()
   const location = useLocation()
   const from = location.state?.from?.pathname || '/'
@@ -67,53 +68,51 @@ const Login = () => {
   };
 
   return (
-    <>
-      <section>
-        <p
-          ref={errRef}
-          className={errMsg ? "errmsg" : "offscreen"}
-          aria-live="assertive"
-        >
-          {errMsg}
-        </p>
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
 
-        <h1>Sign In</h1>
+      <h1>Sign In</h1>
 
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username:</label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
 
-          <input
-            type="text"
-            id="username"
-            ref={userRef}
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-            required
-          />
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
 
-          <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password:</label>
 
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPwd(e.target.value)}
-            value={pwd}
-            required
-          />
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          value={pwd}
+          required
+        />
 
-          <button>Sign In</button>
-        </form>
+        <button>Sign In</button>
+      </form>
 
-        <p>
-          Do not have an account yet?
-          <br />
-          <span className="line">
-            {/* put router link here */}
-            <a href="#">Sign Up</a>
-          </span>
-        </p>
-      </section>
-    </>
+      <p>
+        Do not have an account yet?
+        <br />
+        <span className="line">
+          {/* put router link here */}
+          <a href="#">Sign Up</a>
+        </span>
+      </p>
+    </section>
   );
 };
 
--- src/pages/Unauthorized/index.jsx
+++ src/pages/Unauthorized/index.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const Unauthorized = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => navigate(-1);
+
   return (
     <>
       <section>
         <h1>Unauthorized.</h1>
 
-        <p>Unauthorized</p>
+        <p>You do not have access to the requested page.</p>
+
+        <div className="flexGrow">
+          <button onClick={handleGoBack}>Go Back.</button>
+        </div>
       </section>
     </>
   );
