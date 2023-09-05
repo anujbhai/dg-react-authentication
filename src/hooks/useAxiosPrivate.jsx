7fd3b005fd0b8bf5c70bf5798eba77d16cf1ce90
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2023-09-05 23:08:51 +0530

    dg-react-auth-04-jwt-refresh-token-cookies-axios - refresh access token.

---
+++ src/hooks/useAxiosPrivate.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react"
+import { axiosPrivate } from "../api/axios"
+import useAuth from "./useAuth"
+import useRefreshToken from "./useRefreshTokens"
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken()
+  const { auth } = useAuth()
+
+  useEffect(() => {
+    const requestInterceptor = axiosPrivate.interceptors.request.use(
+      config => {
+        if (!config.headers['Authorization']) {
+          config.headers['Authorization'] = `Bearer ${auth?.accessToken}`
+        }
+
+        return config
+      },
+      (err) => Promise.reject(err)
+    )
+
+    const responseInterceptor = axiosPrivate.interceptors.response.use(
+      response => response,
+      async (err) => {
+        const prevReq = err?.config
+
+        if (err?.response?.status === 403 && !prevReq?.sent) {
+          prevReq.sent = true
+
+          const newAccessToken = await refresh()
+
+          prevReq.headers['Authorization'] = `Bearer ${newAccessToken}`
+
+          return axiosPrivate(prevReq)
+        }
+
+        (err) => Promise.reject(err)
+      }
+    )
+
+    // cleanup on unmount
+    return () => {
+      axiosPrivate.interceptors.response.eject(responseInterceptor)
+    }
+  }, [auth, refresh])
+
+  return axiosPrivate
+}
+
+export default useAxiosPrivate
+
