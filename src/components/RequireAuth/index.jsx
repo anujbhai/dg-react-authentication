cf46c6c98910dd473df657881e59ae6519703393
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2023-09-15 23:34:52 +0530

    dg-react-auth-07-best-practices - final changes.

--- src/components/RequireAuth/index.jsx
+++ src/components/RequireAuth/index.jsx
@@ -2,15 +2,22 @@ import PropTypes from 'prop-types'
 import { useLocation, Navigate, Outlet } from 'react-router-dom'
 
 import useAuth from '../../hooks/useAuth'
+import jwtDecode from 'jwt-decode'
 
 const RequireAuth = props => {
-  const {allowedRoles} = props
+  const { allowedRoles } = props
 
   const { auth } = useAuth()
   const location = useLocation()
 
+  const decoded = auth?.accessToken
+    ? jwtDecode(auth.accessToken)
+    : undefined
+
+  const roles = decoded?.UserInfo?.roles || []
+
   return (
-    auth?.roles?.find(role => allowedRoles?.includes(role))
+    roles.find(role => allowedRoles?.includes(role))
       ? <Outlet />
       : auth?.user
         ? <Navigate to="/unauthorized" state={{ from: location }} replace />
